4997e9bfd3252e7440fe81782e00284eea002f30
yassir benjilali <benjilaliyassir@forleaders.net>
2023-10-23 18:37:13 +0100

    change config personae to gsheet

--- pages/Personaes_config.py
+++ pages/Personaes_config.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 
-uploaded_personaes = st.file_uploader("Upload list of personaes")
+st.cache_data.clear()
+conn_pers = st.experimental_connection("gsheets", type=GSheetsConnection, ttl=1)
+df=conn_pers.read(worksheet="personae")
+st.dataframe(df)
 
-if uploaded_personaes is not None:
-    df=pd.read_excel(uploaded_personaes,index_col="Personaes")
-    st.dataframe(df)
+update=st.button ("update personae")
 
+if update:
     #prepare the prompt
-    df['customer_persona']="You are a responding to a call from a sales person. "
+    df['customer_persona']="You are a customer responding to a call from a sales person. "
     df['customer_persona']+="You are in the industry of "+ df['Customer industry'] + "."
     df['customer_persona']+=" You have the following position in the company: "+df['Customer position']+"."
     df['customer_persona'] += " The size of your company is " + df['Company size'] +"."
@@ -18,4 +21,15 @@ if uploaded_personaes is not None:
     df['customer_persona'] += " your main personality trait are "+ df['Key personality traits'] +"."
     df['customer_persona'] += " you respond briefly to the question. you are a customer not an assistant "
 
-    df.to_csv('data/personaes.csv',index=True)
+    df['email_persona']="You are a customer responding to an email from a sales person. "
+    df['email_persona']+="You are in the industry of "+ df['Customer industry'] + "."
+    df['email_persona']+=" You have the following position in the company: "+df['Customer position']+"."
+    df['email_persona'] += " The size of your company is " + df['Company size'] +"."
+    df['email_persona'] += " Your Existing technical solutions are: " + df['Technical informations'] +"."
+    df['email_persona'] += " The main pain points in your business are "+ df['Customer pain points'] +"."
+    df['email_persona'] += " your decision making factors are " + df['Decision factors'] +"."
+    df['email_persona'] += " your main personality trait are "+ df['Key personality traits'] +"."
+    #df['email_persona'] += " you respond briefly to the question. you are a customer not an assistant "
+
+    conn_pers.update(worksheet="personae",data=df)
+    st.write("Evaluation stored with success")
