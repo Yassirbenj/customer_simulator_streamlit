f6e7a85258f47d08b55c463b404e634aca5a1911
yassir benjilali <benjilaliyassir@forleaders.net>
2023-10-25 12:33:12 +0100

    add whisper speech to text

--- pages/speech_to_text.py
+++ pages/speech_to_text.py
@@ -6,6 +6,6 @@ openai_api_key = st.secrets["openai"]
 
 audio_bytes = audio_recorder(energy_threshold=0.01,pause_threshold=2)
 if audio_bytes:
-    st.audio(audio_bytes, format="audio/wav")
-    transcript = openai.Audio.transcribe(model="whisper-1",file=audio_bytes, openai_api_key=openai_api_key,)
+    audio_wav=st.audio(audio_bytes, format="audio/wav")
+    transcript = openai.Audio.transcribe(model="whisper-1",file=audio_wav, openai_api_key=openai_api_key,)
     st.write(transcript.text)
