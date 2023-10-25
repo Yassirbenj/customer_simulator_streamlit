df07f4611a9c01d479c03b2523971092f06cf602
yassir benjilali <benjilaliyassir@forleaders.net>
2023-10-25 12:17:05 +0100

    adapt energy threshold to 0.01

--- pages/speech_to_text.py
+++ pages/speech_to_text.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 
-audio_bytes = audio_recorder(energy_threshold=0.8,pause_threshold=2)
+audio_bytes = audio_recorder(energy_threshold=0.01,pause_threshold=2)
 if audio_bytes:
     st.audio(audio_bytes, format="audio/wav")
