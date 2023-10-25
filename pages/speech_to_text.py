c667ecb50cc6b78d52b66b3930d2d5ee9ee11283
yassir benjilali <benjilaliyassir@forleaders.net>
2023-10-25 12:48:54 +0100

    try with speech recognition

--- pages/speech_to_text.py
+++ pages/speech_to_text.py
@@ -4,15 +4,13 @@ import openai
 import speech_recognition as sr
 
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
-    st.audio(audio)
 
-#openai_api_key = st.secrets["openai"]
 
-#audio_bytes = audio_recorder(energy_threshold=0.01,pause_threshold=2)
-#if audio_bytes:
-#    st.audio(audio_bytes, format="audio/wav")
+openai_api_key = st.secrets["openai"]
+
+audio_bytes = audio_recorder(energy_threshold=0.01,pause_threshold=2)
+if audio_bytes:
+    st.audio(audio_bytes, format="audio/wav")
+    st.write(r.recognize_whisper_api(audio_bytes, api_key=openai_api_key))
 #    transcript = openai.Audio.transcribe(model="whisper-1",file=audio_wav, api_key=openai_api_key)
 #    st.write(transcript.text)
