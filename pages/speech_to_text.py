d4103fe39c5d631a094f8b7b9cac3d37f6ffb9e0
yassir benjilali <benjilaliyassir@forleaders.net>
2023-10-25 12:35:34 +0100

    api key speechtotext

--- pages/speech_to_text.py
+++ pages/speech_to_text.py
@@ -7,5 +7,5 @@ openai_api_key = st.secrets["openai"]
 audio_bytes = audio_recorder(energy_threshold=0.01,pause_threshold=2)
 if audio_bytes:
     audio_wav=st.audio(audio_bytes, format="audio/wav")
-    transcript = openai.Audio.transcribe(model="whisper-1",file=audio_wav, openai_api_key=openai_api_key,)
+    transcript = openai.Audio.transcribe(model="whisper-1",file=audio_wav, api_key=openai_api_key)
     st.write(transcript.text)
