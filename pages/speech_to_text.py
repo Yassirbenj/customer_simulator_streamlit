aa047c6e93046fb3134aa9adcd560e6798e23ae1
yassir benjilali <benjilaliyassir@forleaders.net>
2023-10-26 11:00:52 +0100

    try with openai whisper

--- pages/speech_to_text.py
+++ pages/speech_to_text.py
@@ -9,8 +9,9 @@ r = sr.Recognizer()
 openai_api_key = st.secrets["openai"]
 
 audio_bytes = audio_recorder(energy_threshold=0.01,pause_threshold=2)
+
 if audio_bytes:
     st.audio(audio_bytes, format="audio/wav")
-    st.write(r.recognize_whisper_api(audio_bytes, api_key=openai_api_key))
-#    transcript = openai.Audio.transcribe(model="whisper-1",file=audio_wav, api_key=openai_api_key)
-#    st.write(transcript.text)
+    #st.write(r.recognize_whisper_api(audio_bytes, api_key=openai_api_key))
+    transcript = openai.Audio.transcribe(model="whisper-1",file=audio_wav, api_key=openai_api_key)
+    st.write(transcript.text)
