a4b7da88a91a6ca177286886fdf351b9077fef95
yassir benjilali <benjilaliyassir@forleaders.net>
2023-10-26 11:22:55 +0100

    try with openai whisper

--- pages/speech_to_text.py
+++ pages/speech_to_text.py
@@ -3,19 +3,27 @@ from audio_recorder_streamlit import audio_recorder
 import openai
 import speech_recognition as sr
 import io
+import tempfile
 
 r = sr.Recognizer()
 
-
 openai_api_key = st.secrets["openai"]
 
-audio_bytes = audio_recorder(energy_threshold=0.01,pause_threshold=2)
+audio_bytes = audio_recorder(energy_threshold=0.01, pause_threshold=2)
 
 if audio_bytes:
     st.audio(audio_bytes, format="audio/wav")
 
-    # Convert bytes to a file-like object
-    audio_file = io.BytesIO(audio_bytes)
+    # Create a temporary WAV file
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_wav:
+        temp_wav.write(audio_bytes)
 
-    transcript = openai.Audio.transcribe(model="whisper-1", file=audio_file, api_key=openai_api_key)
+    # Get the filename of the temporary WAV file
+    wav_filename = temp_wav.name
+
+    # Transcribe using OpenAI
+    transcript = openai.Audio.transcribe(model="whisper-1", file=wav_filename, api_key=openai_api_key)
     st.write(transcript.text)
+
+    # Close and remove the temporary file
+    temp_wav.close()
