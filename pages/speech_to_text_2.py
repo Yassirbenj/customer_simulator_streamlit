0effd7b9afd4b3490dcae3b003a6d8f917523ba0
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-13 18:33:41 +0100

    change audio sensibility

--- pages/speech_to_text_2.py
+++ pages/speech_to_text_2.py
@@ -8,6 +8,7 @@ from audio_recorder_streamlit import audio_recorder
 audio_bytes = audio_recorder(energy_threshold=0.001, pause_threshold=2)
 
 if audio_bytes:
+    st.write("ok")
     st.audio(audio_bytes, format="audio/wav")
 
 def transcript():
