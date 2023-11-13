0bb2b7e8101594800a3b1e54f1e447089f6cec02
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-13 18:28:05 +0100

    test assemblyai with record streamlit

--- pages/speech_to_text_2.py
+++ pages/speech_to_text_2.py
@@ -3,11 +3,18 @@
 
 import assemblyai as aai
 import streamlit as st
+from audio_recorder_streamlit import audio_recorder
 
-aai.settings.api_key = "2ad5176bc2c74bbf9872e42f6feb357c"
-transcriber = aai.Transcriber()
+audio_bytes = audio_recorder(energy_threshold=0.01, pause_threshold=2)
 
-transcript = transcriber.transcribe("https://storage.googleapis.com/aai-web-samples/news.mp4")
-# transcript = transcriber.transcribe("./my-local-audio-file.wav")
+if audio_bytes:
+    st.audio(audio_bytes, format="audio/wav")
 
-st.write(transcript.text)
+def transcript():
+    aai.settings.api_key = "2ad5176bc2c74bbf9872e42f6feb357c"
+    transcriber = aai.Transcriber()
+
+    transcript = transcriber.transcribe("https://storage.googleapis.com/aai-web-samples/news.mp4")
+    # transcript = transcriber.transcribe("./my-local-audio-file.wav")
+
+    st.write(transcript.text)
