c905b684bed668fdac7805e21e9fc4f170323975
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-13 18:41:21 +0100

    configure sensitivity again

--- pages/speech_to_text_2.py
+++ pages/speech_to_text_2.py
@@ -5,7 +5,7 @@ import assemblyai as aai
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 
-audio_bytes = audio_recorder(energy_threshold=0.1, pause_threshold=2)
+audio_bytes = audio_recorder(energy_threshold=0.001, pause_threshold=2)
 
 if audio_bytes:
     st.audio(audio_bytes, format="audio/wav")
