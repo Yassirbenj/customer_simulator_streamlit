3ec7e5aa321c151155f1161dd8e0505d5b143b98
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-13 18:13:19 +0100

    test assemblyai

--- pages/speech_to_text_2.py
+++ pages/speech_to_text_2.py
@@ -1,37 +1,13 @@
+# `pip3 install assemblyai` (macOS)
+# `pip install assemblyai` (Windows)
+
+import assemblyai as aai
 import streamlit as st
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
 
-stt_button = Button(label="Speak", width=100)
+aai.settings.api_key = "2ad5176bc2c74bbf9872e42f6feb357c"
+transcriber = aai.Transcriber()
 
-stt_button.js_on_event("button_click", CustomJS(code="""
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
+transcript = transcriber.transcribe("https://storage.googleapis.com/aai-web-samples/news.mp4")
+# transcript = transcriber.transcribe("./my-local-audio-file.wav")
 
-    recognition.onresult = function (e) {
-        var value = "";
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                value += e.results[i][0].transcript;
-            }
-        }
-        if ( value != "") {
-            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-        }
-    }
-    recognition.start();
-    """))
-
-result = streamlit_bokeh_events(
-    stt_button,
-    events="GET_TEXT",
-    key="listen",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
-
-if result:
-    if "GET_TEXT" in result:
-        st.write(result.get("GET_TEXT"))
+st.write(transcript.text)
