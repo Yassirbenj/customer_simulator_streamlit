7c740966604f697c79532cadc8dd1632458dcf11
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-23 16:43:00 +0100

    try transcript

--- pages/speech_to_text_2.py
+++ pages/speech_to_text_2.py
@@ -7,14 +7,15 @@ from audio_recorder_streamlit import audio_recorder
 
 audio_bytes = audio_recorder(energy_threshold=0.01, pause_threshold=2)
 
-if audio_bytes:
-    st.audio(audio_bytes, format="audio/wav")
-
-def transcript():
+def transcript(file):
     aai.settings.api_key = "2ad5176bc2c74bbf9872e42f6feb357c"
     transcriber = aai.Transcriber()
 
-    transcript = transcriber.transcribe("https://storage.googleapis.com/aai-web-samples/news.mp4")
+    transcript = transcriber.transcribe(file)
     # transcript = transcriber.transcribe("./my-local-audio-file.wav")
 
     st.write(transcript.text)
+
+if audio_bytes:
+    st.audio(audio_bytes, format="audio/wav")
+    transcript(audio_bytes)
