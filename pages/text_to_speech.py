d362c8b2e93774d0de898252f8aba2d7525a5c79
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-27 13:56:15 +0100

    add autoplay

--- pages/text_to_speech.py
+++ pages/text_to_speech.py
@@ -3,6 +3,24 @@ from gtts import gTTS
 import tempfile
 import os
 
+import base64
+
+
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+
+
 def tts(text):
     # Create a temporary file to save the audio
     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio:
@@ -13,7 +31,8 @@ def tts(text):
         tts_response.save(temp_filepath)
 
         # Display the audio using st.audio
-        st.audio(temp_filepath, format="audio/mp3", start_time=0)
+        autoplay_audio(temp_filepath)
+        #st.audio(temp_filepath, format="audio/mp3", start_time=0)
 
     # Delete the temporary file after displaying
     os.remove(temp_filepath)
