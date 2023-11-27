cba18db937a77687fecbfd49b706777e3258326d
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-27 13:34:28 +0100

    add text to speech

--- pages/text_to_speech.py
+++ pages/text_to_speech.py
@@ -1,10 +1,27 @@
 import streamlit as st
 from gtts import gTTS
 import tempfile
+import os
 
 def tts(text):
-    tts_response = gTTS(text, lang="en", slow=False)
-    st.audio(tts_response, format="audio/mp3", start_time=0)
+    # Create a temporary file to save the audio
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio:
+        temp_filepath = temp_audio.name
 
+        # Generate gTTS audio and save it to the temporary file
+        tts_response = gTTS(text, lang="en", slow=False)
+        tts_response.save(temp_filepath)
 
-tts("My name is yassir")
+        # Display the audio using st.audio
+        st.audio(temp_filepath, format="audio/mp3", start_time=0)
+
+    # Delete the temporary file after displaying
+    os.remove(temp_filepath)
+
+# Example usage
+text_to_speak = st.text_input("Enter text to convert to speech:")
+if st.button("Generate Speech"):
+    if text_to_speak:
+        tts(text_to_speak)
+    else:
+        st.warning("Please enter some text to convert to speech.")
