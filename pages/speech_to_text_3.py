f4e424a25c9e129b63ec6d4ab488c1d3dc8d377c
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-27 12:19:41 +0100

    sr test

--- pages/speech_to_text_3.py
+++ pages/speech_to_text_3.py
@@ -6,6 +6,7 @@ import tempfile
 openai_api_key = st.secrets["openai"]
 
 audio_bytes = audio_recorder(energy_threshold=0.01, pause_threshold=2)
+
 if audio_bytes:
     with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio_file:
         temp_audio_file.write(audio_bytes)
@@ -16,7 +17,7 @@ if audio_bytes:
         audio = r.record(source)  # read the entire audio file
     # recognize speech using Whisper API
     try:
-        st.write(f"Whisper API thinks you said {r.recognize_whisper_api(audio_bytes, api_key=openai_api_key)}")
+        st.write(f"Whisper API thinks you said {r.recognize_whisper_api(audio, api_key=openai_api_key)}")
     except sr.RequestError as e:
         st.write("Could not request results from Whisper API")
     st.audio(audio_bytes, format="audio/wav")
