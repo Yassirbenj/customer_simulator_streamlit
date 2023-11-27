25dc55b0a0ba764dec7a458465e5d48fe4bcff85
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-27 22:41:20 +0100

    normalize

--- pages/test_hume.py
+++ pages/test_hume.py
@@ -34,7 +34,7 @@ def hume_prosody():
 
 def json_norm(json_data):
     df0=pd.json_normalize(json_data)
-    df1=pd.json_normalize(df0["results.predictions"])
+    df1=pd.json_normalize(df0["results.predictions"][0])
     return df1
 
 
