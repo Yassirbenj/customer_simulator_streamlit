55a789d3fcf946f10b1278028b8f186409db0259
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-27 23:29:03 +0100

    normalize

--- speech_to_text_4.py
+++ speech_to_text_4.py
@@ -28,25 +28,4 @@ def stxt(key):
             except sr.RequestError as e:
                 response="Could not request results from Whisper API"
                 #st.write(response)
-        #hume
-        filepaths = [temp_audio_filename]
-        client = HumeBatchClient(hume_key)
-        config = ProsodyConfig(granularity="conversational_turn")
-        job = client.submit_job(None, [config], files=filepaths)
-        st.write ("Emotion analysis Running...")
-        details = job.await_complete()
-        pred=job.get_predictions()
-        df=json_norm(pred)
-        st.dataframe(df)
         return response
-
-def json_norm(json_data):
-    data=[]
-    df0=pd.json_normalize(json_data)
-    df1=pd.json_normalize(df0["results.predictions"][0])
-    df2=pd.json_normalize(df1["models.prosody.grouped_predictions"][0])
-    df3=pd.json_normalize(df2["predictions"][0])
-    df4=pd.json_normalize(df3["emotions"][0])
-    df4.sort_values(by="score",ascending=False,inplace=True)
-    df5=df4.iloc[:2,:]
-    return df5
