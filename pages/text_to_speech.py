6646c8b640eecb46407f0c2baface648e02aff0f
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-27 13:25:39 +0100

    add text to speech

--- pages/text_to_speech.py
+++ pages/text_to_speech.py
@@ -5,14 +5,12 @@ import tempfile
 def tts(text):
     tts_response = gTTS(text, lang="en", slow=False)
     if tts_response:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio_file:
-            temp_audio_file.write(tts_response)
-            temp_audio_filename = temp_audio_file.name
-
-        audio_bytes = temp_audio_filename.read()
+        my_file_name = text[0:20]
+        tts.save(f"temp/{my_file_name}.mp3")
+        audio_file = open(f"temp/{my_file_name}.mp3", "rb")
+        audio_bytes = audio_file.read()
         st.markdown("Customer talking:")
         st.audio(audio_bytes, format="audio/mp3", start_time=0)
 
-input=st.text_input("Text")
-if input:
-    tts("Test")
+
+tts("My name is yassir")
