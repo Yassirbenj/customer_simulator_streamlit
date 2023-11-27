f95ed4948ba9c1bf9c89a0353ebc4014e5ef7f95
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-27 17:13:07 +0100

    hide text to speech

--- text_to_speech.py
+++ text_to_speech.py
@@ -6,29 +6,28 @@ import os
 import base64
 
 
-
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
 
 
 def tts(text):
-    def autoplay_audio(file_path: str):
-        with open(file_path, "rb") as f:
-            data = f.read()
-            b64 = base64.b64encode(data).decode()
-            md = f"""
-                <audio controls autoplay="true">
-                <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-                </audio>
-                """
-            st.markdown(
-                md,
-                unsafe_allow_html=True,
-            )
     # Create a temporary file to save the audio
     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio:
         temp_filepath = temp_audio.name
 
         # Generate gTTS audio and save it to the temporary file
-        tts_response = gTTS(text, lang="en", slow=False)
+        tts_response = gTTS(text, lang="en", slow=False,tld="us")
         tts_response.save(temp_filepath)
 
         # Display the audio using st.audio
