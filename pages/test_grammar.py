5fbf59ffca22d68c3ec1dc569f4f18b6c7ea34bc
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-30 10:28:15 +0100

    remove punctuation

--- pages/test_grammar.py
+++ pages/test_grammar.py
@@ -1,9 +1,13 @@
 import nltk
 import streamlit as st
+import string
 
 nltk.download('punkt')
 discussion=st.text_area("input your paragraph")
 if discussion:
+    #remove punctuation
+    for punctuation in string.punctuation:
+        discussion = discussion.replace(punctuation,'')
     words=nltk.word_tokenize(discussion)
     st.title("list of words")
     st.write(words)
