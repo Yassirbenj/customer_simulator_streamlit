b71ad27531b28d3ae90b181d916ef1a88ae00f4e
yassir benjilali <benjilaliyassir@forleaders.net>
2023-11-30 10:31:02 +0100

    remove start of sentences

--- pages/test_grammar.py
+++ pages/test_grammar.py
@@ -8,6 +8,10 @@ if discussion:
     #remove punctuation
     for punctuation in string.punctuation:
         discussion = discussion.replace(punctuation,'')
+    #remove sales person and customer
+    discussion=discussion.replace("Sale person: ",'')
+    discussion=discussion.replace("Customer: ",'')
+    #tokenize
     words=nltk.word_tokenize(discussion)
     st.title("list of words")
     st.write(words)
