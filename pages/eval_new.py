e667c6ac37cce5b74309ad68530f870419ae3e30
yassir benjilali <benjilaliyassir@forleaders.net>
2023-12-09 16:38:37 +0100

    read a text file

--- pages/eval_new.py
+++ pages/eval_new.py
@@ -174,7 +174,8 @@ def scoring_eval():
     uploaded_file = st.file_uploader("Choose a evaluation grid file")
     if uploaded_file is not None:
         file_contents = uploaded_file.read()
-        st.write(file_contents)
+        decoded_response = file_contents.decode('utf-8')
+        st.write(decoded_response)
 
 #config_persona()
 #main()
