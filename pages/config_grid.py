25c2928323cdb152c9a625cfa233f5c612c925de
yassir benjilali <benjilaliyassir@forleaders.net>
2023-12-11 18:03:11 +0100

    config

--- pages/config_grid.py
+++ pages/config_grid.py
@@ -8,8 +8,8 @@ def main():
 def scoring_eval():
     uploaded_file = st.file_uploader("upload a evaluation grid file")
     if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-        st.write(df.iloc[:,1])
+        df = pd.read_csv(uploaded_file,index_col=0)
+        st.write(df.iloc[:,0])
         #return df[]
 
 main()
