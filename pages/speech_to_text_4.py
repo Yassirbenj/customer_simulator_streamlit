9a8d2c42aefcf1e0c8d1d8976fa22dc6f015d597
yassir benjilali <benjilaliyassir@forleaders.net>
2023-12-15 17:07:53 +0100

    reconaissance fr

--- pages/speech_to_text_4.py
+++ pages/speech_to_text_4.py
@@ -30,3 +30,27 @@ def stxt(key):
                 response="Could not request results from Whisper API"
                 #st.write(response)
         return response
+
+def stxt_fr(key):
+
+    audio_bytes = audio_recorder(energy_threshold=0.01, pause_threshold=2)
+    #audio_bytes = audio_recorder(energy_threshold=(-1.0,1.0), pause_threshold=10)
+
+    if audio_bytes:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio_file:
+            temp_audio_file.write(audio_bytes)
+            temp_audio_filename = temp_audio_file.name
+        # use the audio file as the audio source
+        r = sr.Recognizer()
+        with sr.AudioFile(temp_audio_filename) as source:
+            audio = r.record(source)  # read the entire audio file
+        # recognize speech using Whisper API
+            try:
+                response=r.recognize_whisper_api(audio, api_key=key,language='fr-FR')
+                st.write(f"Transcript: {response}")
+            except sr.RequestError as e:
+                response="Could not request results from Whisper API"
+                #st.write(response)
+        return response
+
+stxt_fr
