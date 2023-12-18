78bfbf1d78f6ae18470a1746208db609ecc4b73a
yassir benjilali <benjilaliyassir@forleaders.net>
2023-12-18 17:27:28 +0100

    masquer le micro avant start

--- pages/Customer_simulation_voice.py
+++ pages/Customer_simulation_voice.py
@@ -46,7 +46,7 @@ def main():
             st.session_state.cost=0
             st.session_state.evals=[]
             st.session_state.status="started"
-            st.experimental_rerun
+            st.experimental_rerun()
         with st.sidebar:
                 st.write(personaes.iloc[personae-1,:-2])
 
