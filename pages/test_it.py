1cd349582c2800477bf365797c2883607464df84
yassir benjilali <benjilaliyassir@forleaders.net>
2023-12-18 17:51:00 +0100

    test_it

--- pages/test_it.py
+++ pages/test_it.py
@@ -1,10 +1,17 @@
 import streamlit as st
 
 st.header("test")
+st.session_state.status="non started"
 
 def main():
-    field=st.text_input("enter an evaluation  field")
-    level=st.selectbox("enter level of expertise",options=("Beginner,Intermediate,Expert"))
-    start=st.button("start")
+    if st.session_state.status=="non started":
+        field=st.text_input("enter an evaluation  field")
+        level=st.selectbox("enter level of expertise",options=("Beginner,Intermediate,Expert"))
+        start=st.button("start")
+        if start:
+            st.session_state.status="started"
+
+    else:
+        st.write("continue")
 
 main()
