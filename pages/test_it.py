61c86c7ed1523cd2e89b325fa2ce0241985b07cc
yassir benjilali <benjilaliyassir@forleaders.net>
2023-12-18 18:30:22 +0100

    test_it

--- pages/test_it.py
+++ pages/test_it.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+
 
 st.header("test")
 st.session_state.status="non started"
@@ -13,7 +17,19 @@ def main():
             #main()
 
     if st.session_state.status=="started":
-        st.write("continue")
+        personae(field,level)
 
-#def personae():
+def personae(field,level):
+    context="You are recruiter asking questions to evaluate competencies of a candidate. "
+    context+=f"ask a multiple choice question to evaluate competency {field} with a level of expertise {level}"
+    key=st.secrets["openai"]
+    llm=OpenAI(openai_api_key=key)
+    template = """Question: You are recruiter asking questions to evaluate competencies of a candidate. ask a multiple choice question to evaluate competency {field} with a level of expertise {level}"""
+    prompt = PromptTemplate(template=template, input_variables=["field","level"])
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    input_list = {"field": field,"level": level}
+    question=llm_chain(input_list)
+    st.write(question)
+
+
 main()
