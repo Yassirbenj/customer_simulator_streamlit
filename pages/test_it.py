53f29256476b36173a7887820b9a805c259227a2
yassir benjilali <benjilaliyassir@forleaders.net>
2023-12-18 18:13:13 +0100

    test_it

--- pages/test_it.py
+++ pages/test_it.py
@@ -4,9 +4,9 @@ st.header("test")
 st.session_state.status="non started"
 
 def main():
-    if st.session_state.status=="non started":
-        placeholder=st.empty()
-        with placeholder.container():
+    placeholder=st.empty()
+    with placeholder.container():
+        if st.session_state.status=="non started":
             field=st.text_input("enter an evaluation  field")
             level=st.selectbox("enter level of expertise",options=("Beginner,Intermediate,Expert"))
             start=st.button("start")
@@ -15,7 +15,7 @@ def main():
                 placeholder.empty()
                 main()
 
-    if st.session_state.status=="started":
-        st.write("continue")
+    #if st.session_state.status=="started":
+    #    st.write("continue")
 
 main()
