a39aaf93ea5ed099a5ac1c13a2e991567fa07ad7
yassir benjilali <benjilaliyassir@forleaders.net>
2023-12-18 17:49:12 +0100

    test_it

--- pages/test_it.py
+++ pages/test_it.py
@@ -5,7 +5,6 @@ st.header("test")
 def main():
     field=st.text_input("enter an evaluation  field")
     level=st.selectbox("enter level of expertise",options=("Beginner,Intermediate,Expert"))
-    if field is not None & level is not None:
-        start=st.button("start")
+    start=st.button("start")
 
 main()
