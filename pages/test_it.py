d61400be64f9cd673819294ecf280bdd979f0fee
yassir benjilali <benjilaliyassir@forleaders.net>
2023-12-18 17:47:45 +0100

    test_it

--- pages/test_it.py
+++ pages/test_it.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-
+st.header("test")
 
 def main():
     field=st.text_input("enter an evaluation  field")
-    level=st.selectbox(options=["Beginner,Intermediate,Expert"])
+    level=st.selectbox("enter level of expertise",options=("Beginner,Intermediate,Expert"))
     if field is not None & level is not None:
         start=st.button("start")
 
