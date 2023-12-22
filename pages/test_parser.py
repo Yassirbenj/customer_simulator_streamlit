82ed7c71228b1f87c7c6d9234f9a3774baec7dc6
yassir benjilali <benjilaliyassir@forleaders.net>
2023-12-22 19:59:33 +0100

    bug validate

--- pages/test_parser.py
+++ pages/test_parser.py
@@ -16,13 +16,12 @@ st.header("test")
 
 
 def main():
-    if st.session_state.status=="non started":
-        field=st.text_input("enter an evaluation  field")
-        level=st.selectbox("enter level of expertise",options=("Beginner","Intermediate","Expert"))
-        start=st.button("start")
-        if start:
-            result=parser2(field,level)
-            st.write(result)
+    field=st.text_input("enter an evaluation  field")
+    level=st.selectbox("enter level of expertise",options=("Beginner","Intermediate","Expert"))
+    start=st.button("start")
+    if start:
+        result=parser2(field,level)
+        st.write(result)
 
 
 
