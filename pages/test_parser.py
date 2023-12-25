969584d54aba46db6268211198095d1d970e3024
yassir benjilali <benjilaliyassir@forleaders.net>
2023-12-25 18:28:18 +0100

    bug validate

--- pages/test_parser.py
+++ pages/test_parser.py
@@ -43,11 +43,12 @@ def main():
             st.header("Select the best option")
             options=[result[1], result[2], result[3]]
             st.session_state.options=options
-            with st.form("options"):
-                st.session_state.response = st.radio('select option',options,index=None)
+            with st.form("option"):
+                response = st.radio('select option',options,index=None)
                 validate = st.form_submit_button("validate")
                 if validate:
                     st.session_state.step=2
+                    st.session_state_response=response
 
     if st.session_state.step==2:
             st.write(st.session_state.step)
